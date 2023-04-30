b7b60e337d1b68961dea71a3ddc092383111dd39
Kosanyak19 <kosvik264@gmail.com>
2023-04-30 22:10:12 +0300

    task2

--- Lesson2/task2/src/__test__/User.test.jsx
+++ Lesson2/task2/src/__test__/User.test.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { shallow } from "enzyme";
 import User from "../User";
+import { getUserData } from "../usersGateway";
+
+jest.mock("../usersGateway", () => {
+  return {
+    getUserData: jest.fn(() => Promise.resolve({})),
+  };
+});
 describe("User", () => {
-  it(`should have <span> with class "user__name" if we give prop "name"`, () => {
-    const component = shallow(<User name={"Emma"} />);
-    expect(component.find(".user__name").exists()).toBe(true);
+  it(`should display nothing if user don't get data`, () => {
+    const component = shallow(<User />);
+    expect(component.find(".user").exists()).toBe(false);
   });
-  it(`should have <span> with class "user__age" if we give prop "age"`, () => {
-    const component = shallow(<User age={21} />);
-    expect(component.find(".user__age").exists()).toEqual(true);
+  it(`should display user with data`, () => {
+    const component = shallow(<User />);
+    component.setState({
+      userDate: {
+        name: "John Doe",
+        location: "New York",
+        avatar_url: "https://example.com",
+      },
+    });
+    expect(component.find(".user")).toMatchSnapshot();
+  });
+  it(`should display nothing if user don't get data`, () => {
+    shallow(<User />);
+
+    expect(getUserData).toBeCalled();
   });
 });
